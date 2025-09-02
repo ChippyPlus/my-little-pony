6fdae2e7a701e345264bc3a1972c468246a596ca
Adam Mathlay <mathlayadam@icloud.com>
2025-09-02 02:16:10 +0200

    tests

--- src/main/kotlin/csv/train.kt
+++ src/main/kotlin/csv/train.kt
@@ -3,13 +3,14 @@ package org.cuttlefish.csv
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import kotlinx.coroutines.runBlocking
 import org.cuttlefish.MultiLayerPerceptron
+import kotlin.time.measureTime
 
 
-private const val epochT = 5
+private const val epochT = 30
 private val mlp = MultiLayerPerceptron(
-	inputSize = 784, hiddenSize = 8, outputSize = 1, learningRate = 0.001, epochs = epochT
+	inputSize = 784, hiddenSize = 32, outputSize = 1, learningRate = 0.001, epochs = epochT
 									  )
-
+private val averageTime = LongArray(epochT)
 fun main() {
 	val dataFlat = mutableListOf<Array<IntArray>>()
 	val data = mutableListOf<IntArray>()
@@ -40,10 +41,13 @@ fun main() {
 		trainingData.add(Pair(row.value.map { it.toDouble() }, listOf(dataNumbers[row.index].toDouble())))
 	}
 
-
-	runBlocking {
-		mlp.train(trainingData, progressCallback, 1)
+	val x = measureTime {
+		runBlocking {
+			mlp.train(trainingData, progressCallback, 1)
+		}
 	}
+	println("Time = $x(${x.inWholeMilliseconds})\nAverage Per Epoch = ${averageTime.sum() / (epochT - 1 /* cuz the 1st is 0 */)}ms")
+
 	mlp.save("NumbersModel.json")
 
 }
@@ -60,13 +64,15 @@ private val progressCallback = { epoch: Int, _: Int, avgError: Double ->
 
 	println(
 		"[ E%-7d/$epochT %.5f%% ] [ A%.16f%% ] [ Time: %.5fs ] [ Error Change: %.20f ] [ E Imprv ${if (errorChange < lastErrorChange) "✅" else "❌"} ] [ Improvement ${if (avgError < lastAvgError) "✅" else "❌"} ] [ Time use ${if (avgError < lastAvgError && elapsedTime < lastElapsedTime) "✅" else "❌"} ]".format(
-			epoch, epoch.toDouble() / epochT * 100, avgError, elapsedTime.toDouble() / 1000, errorChange
-																																																																													 )
+			epoch, epoch.toDouble() / epochT * 100, avgError, elapsedTime.toDouble() / 1000, errorChange)
 		   )
 	lastElapsedTime = elapsedTime
 	lastTime = currentTime
 	lastAvgError = avgError
 	lastErrorChange = errorChange
+	averageTime[epoch - 1] = elapsedTime
+	// I fucking waited three hours to only realise I forgot to minus one oh my goodness its 2 am and I'm tired I just
+	// did so wrong today dont worry you wrote about today its on 2/9
 }
 
 
